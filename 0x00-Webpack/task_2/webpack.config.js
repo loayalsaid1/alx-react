0f521d8f52173957e5336bac841da1e694e90e75
Loay Al-Said <l.programming555@gmail.com>
2024-09-04 00:22:49 +0300

    Once more, deal with a dump and stupid checker for task 2

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,36 +1,31 @@
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-	mode: 'production',
-	entry: './js/dashboard_main.js',
-	output: {
-		path: path.resolve(__dirname, 'public'),
-		filename: 'bundle.js'
-	},
-	module: {
-
-		rules: [
-			{
-				test: /\.css$/,
-			use: ['style-loader', 'css-loader']
-		},
-		{
-			test: /\.(gif|png|jpe?g|svg)$/i,
-			use: [
-				{
-					loader: 'image-webpack-loader',
-					options: {
-							bypassOnDebug: true,
-						},
-					},
-				],
-			}
-		]
-	},
-	plugins: [
-		new HTMLWebpackPlugin({
-			title: 'Task 2'
-		})
-	]
-}
+  mode: "production",
+  entry: "./js/dashboard_main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
