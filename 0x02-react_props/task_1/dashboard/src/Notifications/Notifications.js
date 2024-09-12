76c3b6d3e781d78b2460cc99ca080d89ef7437e4
Loay Al-Said <l.programming555@gmail.com>
2024-09-12 06:05:55 +0300

    Add a function name to Footer.js

---
+++ 0x02-react_props/task_1/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Notifications.css";
+import { getLatestNotification } from "../utils/utils";
+import closeIcon from "../assets/close-icon.png";
+
+
+export default function Notifications() {
+  const handleCloseClick = () => {
+    console.log("Close button has been clicked");
+  };
+
+  return (
+    <div className="Notifications">
+      <p>Here is the list of notifications</p>
+      <ul>
+        <li key="1" data-priority="default">
+          New course available
+        </li>
+        <li key="2" data-priority="urgent">
+          New resume available
+        </li>
+        <li key="3" data-priority="urgent"
+		dangerouslySetInnerHTML={{__html: getLatestNotification()}}
+		>
+        </li>
+      </ul>
+      <button
+        className="close_button"
+        aria-label="Close"
+        type="button"
+        onClick={handleCloseClick}
+      >
+        <img src={closeIcon} alt="Close Icon" />
+      </button>
+    </div>
+  );
+}
