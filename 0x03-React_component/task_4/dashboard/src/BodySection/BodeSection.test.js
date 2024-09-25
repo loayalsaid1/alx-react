b03b0d2e0c413354013e0794a12b2cb9071e009c
Loay Al-Said <l.programming555@gmail.com>
2024-09-25 11:16:27 +0300

    Task 7

---
+++ 0x03-React_component/task_4/dashboard/src/BodySection/BodeSection.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import BodySection from './BodySection';
+
+
+describe('<BodySection />', () => {
+	let wrapper;
+	beforeEach(() => {
+		const element = <BodySection title="Test title">
+			<p>Test child</p>
+		</BodySection>
+		wrapper = shallow(element)
+	})
+
+	it('Renderes correctly the h2 title', () => {
+		expect(wrapper.find('h2').length).toBeGreaterThan(0);
+		expect(wrapper.text()).toMatch('Test title');
+	})
+	it('Renders correctly children passed to it', () => {
+		expect(wrapper.contains(<p>Test child</p>)).toBe(true);
+	})
+})
