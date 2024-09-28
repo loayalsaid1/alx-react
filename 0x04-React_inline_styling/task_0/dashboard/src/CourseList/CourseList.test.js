9d49e458baf890bf25398c51fd40d6d66c7c9f54
Loay Al-Said <l.programming555@gmail.com>
2024-09-28 14:49:40 +0300

    Task 0

---
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseList.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { shallow } from 'enzyme';
+import CourseList from './CourseList';
+import CourseListRow from "./CourseListRow";
+
+
+describe('<CourseList />', () => {
+    let wrapper;
+    const listCourses = [
+        {id: 1, name: 'ES6', credit: 60},
+        {id: 2, name: 'Webpack', credit: 20},
+        {id: 3, name: 'React', credit: 40}
+    ];
+
+    beforeEach(() => {
+        wrapper = shallow(<CourseList listCourses={listCourses}/>);
+    });
+
+    it('renders without crashing with empty or not at all listCourses', () => {
+        shallow(<CourseList />);
+        shallow(<CourseList listCourses={[]}/>);
+    })
+
+    it('renders correct count of rows', () => {
+        expect(wrapper.find(CourseListRow)).toHaveLength(5);
+    })
+})
