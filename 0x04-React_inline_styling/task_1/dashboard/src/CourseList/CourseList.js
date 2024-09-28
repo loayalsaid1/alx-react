3345f13a9d2ef0f82078f8ba2d0d47a6f95f464c
Loay Al-Said <l.programming555@gmail.com>
2024-09-28 23:43:47 +0300

    Use aphrodite inline styles with CourseList table

--- 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
 import CourseShape from './CourseShape';
 import CourseListRow from './CourseListRow';
 import './CourseList.css';
+
+
+const styles = StyleSheet.create({
+    CourseListTable: {
+        width: "100%",
+        textAlign: "left",
+        border: "1px solid lightgrey",
+    }
+});
 
 function CourseList ({ listCourses = [] }) {
     const rows = listCourses.length
@@ -16,7 +26,7 @@ function CourseList ({ listCourses = [] }) {
         : <CourseListRow textFirstCell='No course available yet' />;
 
     return (
-        <table id="CourseList">
+        <table id="CourseList" className={css(styles.CourseListTable)} > 
             <thead>
                 <CourseListRow textFirstCell="Available courses" isHeader={true} />
                 <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
@@ -33,4 +43,3 @@ CourseList.propTypes = {
 }
 
 export default CourseList;
-
