7089932afd85a421d14792c49ee41afc0ec77f65
Loay Al-Said <l.programming555@gmail.com>
2024-09-29 00:00:30 +0300

    Use inline styling with aphrodite in Header.js

--- 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -1,12 +1,36 @@
 import React from "react";
-import './Header.css';
+import { StyleSheet, css } from 'aphrodite';
 import logo from '../assets/holberton_logo.png';
+
+const cssVars = {
+  mainColor: '#e0354b'
+}
+const styles = StyleSheet.create({
+  AppHeader: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+
+    borderBottom: '.3rem solid ' + cssVars.mainColor,
+    height: '15rem',
+  },
+
+  logo: {
+    height: '100%',
+    objectFit: 'contain'
+  },
+
+  header: {
+    color: cssVars.mainColor,
+    textAlign: 'left'
+  }
+})
 
 export default function Header() {
   return (
-    <div className="App-header">
-      <img src={logo} className="logo" alt="Holberton Logo" />
-      <h1>School dashboard</h1>
+    <div className={css(styles.AppHeader)}>
+      <img src={logo} className={css(styles.logo)} alt="Holberton Logo" />
+      <h1 className={css(styles.header)} >School dashboard</h1>
     </div>
   );
 }
