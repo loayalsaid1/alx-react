0ad80c1cc3001325c4c98533bccccd1f95f7b8da
Loay Al-Said <l.programming555@gmail.com>
2024-10-09 20:55:56 +0300

    8-seq.js

---
+++ 0x06-React_Immutable/8-seq.js
@@ -0,0 +1,15 @@
+import { Seq } from 'immutable';
+
+export default function printBestStudents(scores) {
+  const capitalizeWord = (word) => word.charAt(0).toUpperCase() + word.slice(1);
+
+  const bestStudents = Seq(scores)
+    .filter((entry) => entry.score >= 70)
+    .map((entry) => ({
+      ...entry,
+      firstName: capitalizeWord(entry.firstName),
+      lastName: capitalizeWord(entry.lastName),
+    }));
+
+  console.log(bestStudents.toJS());
+}
